67348b49bde723efdeccae5e049c6732ce9cd0ac
Ahmed Sayed <ahmed.sayed.hassanien@gmail.com>
2020-07-14 14:48:44 +0200

    bnbnbnb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ReportAng';
+  reportServer: string = 'http://ssrs-testserver.westus2.cloudapp.azure.com/reportserver';
+  reportUrl: string = 'SampleWithParameters';
+  showParameters: string = "true"; 
+  parameters: any = {
+   "SampleStringParameter": null,
+   "SampleBooleanParameter" : false,
+   "SampleDateTimeParameter" : "9/1/2017",
+   "SampleIntParameter" : 1,
+   "SampleFloatParameter" : "123.1234",
+   "SampleMultipleStringParameter": ["Parameter1", "Parameter2"]
+   };
+  language: string = "en-us";
+  width: number = 100;
+  height: number = 100;
+  toolbar: string = "true";
 }
