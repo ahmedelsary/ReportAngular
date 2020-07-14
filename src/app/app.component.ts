bc323540e707d92416161b0e3796c308d3116837
Fady.nashat.manhary <Fady.nashat.manhary@outlook.com>
2020-07-14 17:20:13 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  reportServer: string = 'http://ssrs-testserver.westus2.cloudapp.azure.com/reportserver';
-  reportUrl: string = 'SampleWithParameters';
+ reportServer: string = 'http://9.111.114.54:5050/ReportServer_SSRS';
+  reportUrl: string = 'My Reports/Report1';
   showParameters: string = "true"; 
-  parameters: any = {
-   "SampleStringParameter": null,
-   "SampleBooleanParameter" : false,
-   "SampleDateTimeParameter" : "9/1/2017",
-   "SampleIntParameter" : 1,
-   "SampleFloatParameter" : "123.1234",
-   "SampleMultipleStringParameter": ["Parameter1", "Parameter2"]
-   };
+  // parameters: any = {
+  //  "SampleStringParameter": null,
+  //  "SampleBooleanParameter" : false,
+  //  "SampleDateTimeParameter" : "9/1/2017",
+  //  "SampleIntParameter" : 1,
+  //  "SampleFloatParameter" : "123.1234",
+  //  "SampleMultipleStringParameter": ["Parameter1", "Parameter2"]
+  //  };
+  
+   parameters: any = {
+   "Code": 120027,
+
+    };
   language: string = "en-us";
   width: number = 100;
   height: number = 100;
